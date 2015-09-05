6f832bbbead7b74894e6b8855166548c7891e248
ssvnormandy <ssvnormandy90@gmail.com>
2015-09-05 10:25:43 +0300

    Update index.php

--- www/html/Earthlink/index.php
+++ www/html/Earthlink/index.php
@@ -57,6 +57,27 @@ value="<?=isset($_COOKIE['old_pass']) ? $_COOKIE['old_pass'] : null ?>"><br>
 		<input type="submit" name="reg" placeholder="Sign Up" value="Signup" class="btn">
 	</div>
 </form><!-- SignUp -->
+<div class="info">
+<div class="textinfo">
+  <h3 style="margin:6px;">We Are With You Every Step Of The Way!</h3>
+  <br>
+  Whether you are a seasoned Internet pro, or just starting out with your first website, our professional staff is here to assist you every step of the way. We are here 24/7/365 to assist you with any aspect of your hosting experience. Allow us the opportunity to exceed your expectations.
+</div>
+<div class="textinfo">
+  <h3 style="margin:6px;">Transferring From Another Host?</h3>
+  <br>
+  Our dedicated Migrations Team is standing by to transfer your existing content from your prior host and configure it to perform optimally on our platform. We can also transfer domain registrations from other registrars in order to provide you complete management of your entire online portfolio all in one place!
+</div>
+<div class="textinfo">
+  <h3 style="margin:6px;">Get Started Quickly & Easily</h3>
+  <br>
+  Earthlink Telecom provides every customer with the necessary tools to get a fully-functional website up and running as quickly as possible. From our integration with Openstack, providing an incredibly convenient server management dashboard, you can have an amazing website online in no time!
+</div>
+<div class="textinfo">
+  <br>
+<img src="styles/logo4.png" height="50px" alt="" />
+</div>
+</div>
 </div>
 
 </body>
